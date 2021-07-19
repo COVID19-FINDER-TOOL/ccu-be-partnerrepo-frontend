a59e2142fae857a61434a253c6d717e470efb56f
Ankuksh Mittal <ankush.mittal@soprasteria.com>
2021-07-19 17:33:24 +0530

    build folder

---
+++ build/precache-manifest.70dd7d392a6296ea4ce765849ab0545a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f136866eb4aac381286293aba930363",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/index.html"
+  },
+  {
+    "revision": "4786f78a48aa49eeb085",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/css/2.17b531b1.chunk.css"
+  },
+  {
+    "revision": "31237e50a251639c7866",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/css/main.4674392f.chunk.css"
+  },
+  {
+    "revision": "4786f78a48aa49eeb085",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/js/2.8d337d6d.chunk.js"
+  },
+  {
+    "revision": "cf01d315609437d251994bc2c9ba5c8e",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/js/2.8d337d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31237e50a251639c7866",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/js/main.b57e0e13.chunk.js"
+  },
+  {
+    "revision": "9a3deb93699b4616a962",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/js/runtime-main.226754b8.js"
+  },
+  {
+    "revision": "223505e463ba3dae2f8fd342598559a4",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/Action_Plan-min.223505e4.png"
+  },
+  {
+    "revision": "84265c46a38c02bb22aaa99e81a229e0",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/Background-min.84265c46.png"
+  },
+  {
+    "revision": "7580d5fa49e286a607f53427418de84e",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/Image 7.7580d5fa.png"
+  },
+  {
+    "revision": "5e248caf5b91259d87a5c7548a869e01",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/Landing page -Oranges.5e248caf.jpg"
+  },
+  {
+    "revision": "d21d04c3e6c70087b3a5f1e97985b707",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/Magnifying glass buterflies.d21d04c3.jpg"
+  },
+  {
+    "revision": "9f57f72cba728c08a6f3f5543efd3ba8",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/Spinner-1s-200px.9f57f72c.svg"
+  },
+  {
+    "revision": "b6ed623f78699cb60da7588109e9e9d6",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/Spinner-1s-200px.b6ed623f.gif"
+  },
+  {
+    "revision": "707e9a1fb8c66a5fd24e96ddf96cfb7e",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/Support_finder_logo2x.707e9a1f.png"
+  },
+  {
+    "revision": "6f2544e36fe1cccbfc62c79ef62252d6",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/Thank-you-min.6f2544e3.png"
+  },
+  {
+    "revision": "4ceacc22a6c8ba9c416ce734c4caefae",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/check-square.4ceacc22.svg"
+  },
+  {
+    "revision": "4eeab1ce769408666b8ec7142598468e",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/hear_from_others.4eeab1ce.png"
+  },
+  {
+    "revision": "163768490dc7a1a1b9ceb00912e78d36",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/newtab.16376849.svg"
+  },
+  {
+    "revision": "76d15e830ddbb55c7d977eb2d5c5363e",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/podcast.76d15e83.png"
+  },
+  {
+    "revision": "eddba2ab9a114d56ade6cb1881e15e34",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/reading.eddba2ab.png"
+  },
+  {
+    "revision": "e740c24e773e84468bc115aeca9a140a",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/tell_uss-min.e740c24e.png"
+  },
+  {
+    "revision": "5c6db3b22835c09eda8a85e04bb42920",
+    "url": "https://jolly-beach-01e73f303.azurestaticapps.net/static/media/video.5c6db3b2.png"
+  }
+]);
