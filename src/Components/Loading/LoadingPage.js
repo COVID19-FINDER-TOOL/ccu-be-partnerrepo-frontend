6d22b689989bf4e82faa3ff40adc3c3216e29667
Ankuksh Mittal <ankush.mittal@soprasteria.com>
2021-07-21 11:48:36 +0530

    deploying to static web app

--- src/Components/Loading/LoadingPage.js
+++ src/Components/Loading/LoadingPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 class Loading extends React.Component {
     render(){
-        return(<div></div>)
+        return(<div>Loadind....</div>)
     }
 }
 export default Loading;
