ba44be6022c0addd3153a67f9adb3306fbaa8bcc
Ankuksh Mittal <ankush.mittal@soprasteria.com>
2021-07-28 13:04:02 +0530

    adding bottom text to ccu

--- src/Components/Chatbot/Chatbot.js
+++ src/Components/Chatbot/Chatbot.js
@@ -688,7 +688,8 @@ class Chatbot extends React.Component {
 
     splitQuestionData = (topic) => {
         const text = this.state.data.answer;
-        const textarray = text.split("\n");
+        const tempText = text.split("\n\n######");
+        const textarray = topic == 4 ? tempText[0].split("\n") : text.split("\n") ;
         const { CREATEJOURNEY } = this.props.payload
         var { responseStack, questionStack, selectedJourneys } = CREATEJOURNEY ? CREATEJOURNEY : []
         var texts = [];
@@ -705,6 +706,7 @@ class Chatbot extends React.Component {
         }
         
         if (topic == 4) {
+            
             textarray.map((x) => {
                 if (x.match(/^\d/)) {
                     links.push(x)
@@ -746,7 +748,9 @@ class Chatbot extends React.Component {
                                 })
                             }
                         </div>
+                        <h6 style={{paddingTop:10, marginLeft:-5}} >{tempText[tempText.length-1]}</h6>
                     </div>
+
                     <div style={{ display: !this.state.showActionPlan && this.state.showNextJourney && this.state.selectedJourneys.length ? "block" : "none"  }}>
                         {this.gotoNextJourney()}
                     </div>
